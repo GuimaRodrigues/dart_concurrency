9bb1af2c323d11bc400280bf5feda28351152577
Guilherme Rodrigues Martins <guilherme.martinseng@hotmail.com>
2023-12-02 18:17:16 -0300

    ajuste matriz

--- bin/trabalho_concorrencia.dart
+++ bin/trabalho_concorrencia.dart
@@ -1,26 +1,54 @@
 import 'dart:isolate';
+import 'dart:math';
 
 void main(List<String> arguments) async {
-  List<int> novaMatrix = [0, 0, 0];
-  dynamic matrix1 = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [4, 3, 2, 1]
-  ];
+  int tamanhoMatriz = 100;
+  List<int> novaMatrixParalelo = List.generate(tamanhoMatriz, (index) => 0);
+  List<int> novaMatrixSequencial = List.generate(tamanhoMatriz, (index) => 0);
+  dynamic matrix1 = generateMatrix(tamanhoMatriz, tamanhoMatriz);
 
-  dynamic matrix2 = [11, 12, 13, 14];
+  List<int> matrix2 = [];
+  for (int i = 0; i < tamanhoMatriz; i++) {
+    matrix2.add(Random().nextInt(50));
+  }
   print("Matriz original $matrix1");
+  print("");
   print("Matriz que vai multiplicar a matriz original $matrix2");
-  ToyExemplo toyExemplo = ToyExemplo();
-  await toyExemplo.multiplicaMatriz(novaMatrix, matrix1, matrix2);
+  print("");
 
-  print("Resultado Final $novaMatrix");
+  ToyExemplo toyExemplo = ToyExemplo();
+
+  Stopwatch tempoSequencial = new Stopwatch()..start();
+  await toyExemplo.multiplicaMatrizSequencial(
+      novaMatrixSequencial, matrix1, matrix2);
+  print('Tempo de execução sequencial ${tempoSequencial.elapsed}');
+
+  Stopwatch tempoParalelo = new Stopwatch()..start();
+  await toyExemplo.multiplicaMatrizParalelo(
+      novaMatrixParalelo, matrix1, matrix2);
+  print('Tempo de execução paralela ${tempoParalelo.elapsed}');
+  print("");
+
+  print("Resultado Final $novaMatrixParalelo");
+}
+
+generateMatrix(int rows, int cols) {
+  List<List<int>> matrix = [];
+
+  for (int i = 0; i < rows; i++) {
+    matrix.add([]);
+    for (int j = 0; j < cols; j++) {
+      matrix[i].add(Random().nextInt(50));
+    }
+  }
+
+  return matrix;
 }
 
 class ToyExemplo {
   dynamic matrix2 = [11, 12, 13, 14];
 
-  multiplicaMatriz(novaMatrix, matrix1, matrix2) async {
+  multiplicaMatrizParalelo(novaMatrixParalelo, matrix1, matrix2) async {
     dynamic retorno;
     for (int i = 0; i < matrix1.length; i++) {
       var isolateListener = ReceivePort();
@@ -34,43 +62,62 @@ class ToyExemplo {
           'isolate': isolateListener.sendPort,
           'matrix1': matrix1[i],
           'matrix2': matrix2,
-          'novaMatrix': novaMatrix,
+          'novaMatrix': novaMatrixParalelo,
           'posicaoMatriz': i,
         },
       );
-      novaMatrix[i] = await retorno;
+      novaMatrixParalelo[i] = await retorno;
     }
 
     return await retorno;
   }
 
-  _logicaToy(SendPort message) {
-    var isolatePrivatePort = ReceivePort();
-    message.send(isolatePrivatePort.sendPort);
-
-    isolatePrivatePort.listen((message) {
-      var externalIsolate = message['isolate'];
-      List<int> matrix1 = message['matrix1'];
-      List<int> matrix2 = message['matrix2'];
-      List<int> novaMatrix = message['novaMatrix'];
-      int posicaoMatriz = message['posicaoMatriz'];
-      externalIsolate
-          .send(_multiplicaMatriz(posicaoMatriz, novaMatrix, matrix1, matrix2));
-    });
-  }
-
-  _multiplicaMatriz(posicaoMatriz, novaMatrix, matrix1, matrix2) {
+  multiplicaMatrizSequencial(novaMatrixSequencial, matrix1, matrix2) async {
     int tamanhoMatriz1 = matrix1.length;
     for (int j = 0; j < tamanhoMatriz1; j++) {
-      num soma = 0;
+      for (int k = 0; k < matrix1.length; k++) {
+        var row = matrix1[k];
+        num soma = 0;
 
-      for (int i = 0; i < tamanhoMatriz1; i++) {
-        soma = soma + matrix1[i] * matrix2[i];
-        if (j < matrix1.length - 1) {
-          novaMatrix[j] = soma;
+        for (int i = 0; i < tamanhoMatriz1; i++) {
+          soma = soma + row[i] * matrix2[i];
+          if (j <= row.length - 1) {
+            novaMatrixSequencial[k] = soma;
+          }
         }
       }
     }
-    return novaMatrix[posicaoMatriz];
+    print("Matriz sequencial $novaMatrixSequencial");
+    return novaMatrixSequencial;
   }
+}
+
+_logicaToy(SendPort message) {
+  var isolatePrivatePort = ReceivePort();
+  message.send(isolatePrivatePort.sendPort);
+
+  isolatePrivatePort.listen((message) {
+    var externalIsolate = message['isolate'];
+    List<int> matrix1 = message['matrix1'];
+    List<int> matrix2 = message['matrix2'];
+    List<int> novaMatrix = message['novaMatrix'];
+    int posicaoMatriz = message['posicaoMatriz'];
+    externalIsolate
+        .send(_multiplicaMatriz(posicaoMatriz, novaMatrix, matrix1, matrix2));
+  });
+}
+
+_multiplicaMatriz(posicaoMatriz, novaMatrix, matrix1, matrix2) {
+  int tamanhoMatriz1 = matrix1.length;
+  for (int j = 0; j < tamanhoMatriz1; j++) {
+    num soma = 0;
+
+    for (int i = 0; i < tamanhoMatriz1; i++) {
+      soma = soma + matrix1[i] * matrix2[i];
+      if (j <= matrix1.length - 1) {
+        novaMatrix[j] = soma;
+      }
+    }
+  }
+  return novaMatrix[posicaoMatriz];
 }
