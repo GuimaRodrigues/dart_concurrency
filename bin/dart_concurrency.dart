ea955e0e5604cc9ec17034658612b94e33283cc3
Guilherme Rodrigues Martins <guilherme.martinseng@hotmail.com>
2023-12-06 07:46:22 -0300

    tempo sem passagem de mensagem

--- bin/dart_concurrency.dart
+++ bin/dart_concurrency.dart
@@ -5,6 +5,8 @@ void main(List<String> arguments) async {
   // Criando variaveis
   int tamanhoMatriz = 10, linhas = 5;
 
+  Duration tempoSemConsiderarPassagemMensagem = Duration.zero;
+
   List<num> novaMatrixParalelo =
       List.generate(linhas, (index) => 0, growable: true);
 
@@ -33,7 +35,7 @@ void main(List<String> arguments) async {
   // Execução trecho sequencial
   Stopwatch tempoParalelo = Stopwatch()..start();
   await toyExemplo.multiplicaMatrizParalelo(
-      novaMatrixParalelo, matrix1, matrix2);
+      tempoSemConsiderarPassagemMensagem, novaMatrixParalelo, matrix1, matrix2);
   print('Tempo de execução paralela ${tempoParalelo.elapsed}');
   print("");
 
@@ -55,8 +57,10 @@ generateMatrix(int rows, int cols) {
 }
 
 class ToyExemplo {
-  multiplicaMatrizParalelo(novaMatrixParalelo, matrix1, matrix2) async {
+  multiplicaMatrizParalelo(tempoSemConsiderarPassagemMensagem,
+      novaMatrixParalelo, matrix1, matrix2) async {
     dynamic retorno;
+    // A cada linha é criado um isolate
     for (int i = 0; i < novaMatrixParalelo.length; i++) {
       var isolateListener = ReceivePort();
       retorno = isolateListener.first;
@@ -70,10 +74,18 @@ class ToyExemplo {
           'matrix1': matrix1[i],
           'matrix2': matrix2,
           'novaMatrix': novaMatrixParalelo,
+          'tempoSemConsiderarPassagemMensagem':
+              tempoSemConsiderarPassagemMensagem,
           'posicaoMatriz': i,
         },
       );
-      novaMatrixParalelo[i] = await retorno;
+      novaMatrixParalelo[i] = (await retorno)[0];
+      tempoSemConsiderarPassagemMensagem = (await retorno)[1];
+      if (i == novaMatrixParalelo.length - 1) {
+        print(
+            'Tempo de execução paralela sem considerar passagem de mensagem $tempoSemConsiderarPassagemMensagem');
+        print("");
+      }
     }
   }
 
@@ -106,14 +118,17 @@ _logicaToy(SendPort message) {
     List<int> matrix1 = message['matrix1'];
     List<int> matrix2 = message['matrix2'];
     List<num> novaMatrix = message['novaMatrix'];
+    Duration tempoSemConsiderarPassagemMensagem =
+        message['tempoSemConsiderarPassagemMensagem'];
     int posicaoMatriz = message['posicaoMatriz'];
-    externalIsolate.send(_multiplicaMatriz(
+    externalIsolate.send(_multiplicaMatriz(tempoSemConsiderarPassagemMensagem,
         externalIsolate, posicaoMatriz, novaMatrix, matrix1, matrix2));
   });
 }
 
-_multiplicaMatriz(
-    externalIsolate, posicaoMatriz, novaMatrix, matrix1, matrix2) {
+_multiplicaMatriz(tempoSemConsiderarPassagemMensagem, externalIsolate,
+    posicaoMatriz, novaMatrix, matrix1, matrix2) {
+  Stopwatch tempoCodigo = Stopwatch()..start();
   int tamanhoMatriz1 = novaMatrix.length;
   for (int j = 0; j < tamanhoMatriz1; j++) {
     num soma = 0;
@@ -125,10 +140,13 @@ _multiplicaMatriz(
       }
     }
   }
+  Duration tempoPorIsolate = tempoCodigo.elapsed;
+  tempoSemConsiderarPassagemMensagem =
+      tempoSemConsiderarPassagemMensagem + tempoPorIsolate;
 
   print(
-      "Isolate $posicaoMatriz  e nova parte de matriz ${novaMatrix[posicaoMatriz]}");
+      "Isolate $posicaoMatriz  e nova parte de matriz ${novaMatrix[posicaoMatriz]} e um tempo de execução por isolate de $tempoPorIsolate");
   print("");
 
-  return novaMatrix[posicaoMatriz];
+  return [novaMatrix[posicaoMatriz], tempoSemConsiderarPassagemMensagem];
 }
